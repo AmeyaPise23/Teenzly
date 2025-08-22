4b52d599794e241c1cab36bce7ebf580abd1fee3
Janvi Pise <janvipise27@gmail.com>
2025-08-22 15:05:31 +0530

    Initial commit - Teenzly microservices structure

---
+++ Community-service/communityservice/src/test/java/com/teenzly/communityservice/CommunityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teenzly.communityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
